dc7a2c2c112fd5bf8ca5dfcd363bd0cb01265523
idic <idic@kcn.ne.jp>
2020-10-18 16:51:08 +0900

    ホームメニュー追加（暫定版）

---
+++ MarchePOS/MarchePOS/ViewModels/HomeMenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MarchePOS.Services.Interfaces;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarchePOS.ViewModels
+{
+    public class HomeMenuViewModel : BindableBase
+    {
+        public List<MenuItem> HomeMenuItems { get; set; }
+
+        public HomeMenuViewModel(IMenuService menuService)
+        {
+            HomeMenuItems = menuService.GetMainMenuItems();
+        }
+    }
+}
